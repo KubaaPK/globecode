3bf5512cea18363334bc492a1775e9b3ffa93258
Kuba <k.szkowrytko@gmail.com>
2017-01-23 15:24:39 +0100

    Initial client-side configuration

---
+++ client/src/app/app.js
@@ -0,0 +1,12 @@
+(function () {
+    'use strict';
+
+    require('angular');
+    require('angular-ui-router');
+
+    angular
+        .module('globeCode', [
+            'ui.router'
+        ]);
+
+})();
