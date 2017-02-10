ecdc092085e83a0f39ac1ce22b4279710dbe33e6
Kuba <k.szkowrytko@gmail.com>
2017-02-10 12:38:40 +0100

    Displaying amount of existing offers

--- client/public/main.js
+++ client/public/main.js
@@ -43385,6 +43385,8 @@ exports.ViewService = ViewService;
                     for(var i = 0; i<res.data.length;i++) {
                         vm.data.push(res.data[i].fields);
                     }
+                    vm.offersAmount = res.data.length;
+                    offerPlural(res.data.length);
                 })
                 .catch(function(err) {
                     console.log(err);
@@ -43405,7 +43407,8 @@ exports.ViewService = ViewService;
                         for(var i = 0; i<res.data.length;i++) {
                             vm.data.push(res.data[i].fields);
                         }   
-                        console.log(vm.data);
+                        vm.offersAmount = res.data.length;
+                        offerPlural(res.data.length);
                     })
                     .catch(function(err) {
                         console.log(err);
@@ -43413,6 +43416,18 @@ exports.ViewService = ViewService;
             });
         }
         
+
+        function offerPlural(offers) {
+            if(offers === 0 ) {
+                vm.offerPlural = "ofert";
+            } else if(offers === 1) {
+                vm.offerPlural = "ofertę";
+            } else if(offers === 2 || offers === 3 || offers === 4) {
+                vm.offerPlural = "oferty";
+            } else {
+                vm.offerPlural = "ofert";
+            }
+        }
 
 
 
--- client/src/app/components/offer.component.js
+++ client/src/app/components/offer.component.js
@@ -20,6 +20,8 @@
                     for(var i = 0; i<res.data.length;i++) {
                         vm.data.push(res.data[i].fields);
                     }
+                    vm.offersAmount = res.data.length;
+                    offerPlural(res.data.length);
                 })
                 .catch(function(err) {
                     console.log(err);
@@ -40,7 +42,8 @@
                         for(var i = 0; i<res.data.length;i++) {
                             vm.data.push(res.data[i].fields);
                         }   
-                        console.log(vm.data);
+                        vm.offersAmount = res.data.length;
+                        offerPlural(res.data.length);
                     })
                     .catch(function(err) {
                         console.log(err);
@@ -48,6 +51,18 @@
             });
         }
         
+
+        function offerPlural(offers) {
+            if(offers === 0 ) {
+                vm.offerPlural = "ofert";
+            } else if(offers === 1) {
+                vm.offerPlural = "ofertę";
+            } else if(offers === 2 || offers === 3 || offers === 4) {
+                vm.offerPlural = "oferty";
+            } else {
+                vm.offerPlural = "ofert";
+            }
+        }
 
 
 
